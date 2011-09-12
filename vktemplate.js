cda6e6ede399f64b4730877a62e8fe7e7aa0ba21
unknown <vkiryukhin@.corp.valueclick.com>
2011-09-12 11:34:42 -0700

    simplified micro-template engine, modified license references and code comments

--- vktemplate.js
+++ vktemplate.js
@@ -1,22 +1,31 @@
 /**
-* smartupdater - jQuery Plugin
+* vkTemplate - jQuery Plugin
 *  
-* Version - 0.2.00.alpha
+* Version - 0.3.00.alpha
 * Copyright (c) 2010 - 2011 Vadim Kiryukhin
 * vkiryukhin @ gmail.com
-* 
 * http://www.eslinstructor.net/vktemplate/
-*
+* 
 * Dual licensed under the MIT and GPL licenses:
 *   http://www.opensource.org/licenses/mit-license.php
 *   http://www.gnu.org/licenses/gpl.html
 *
+* Includes Simple JavaScript Templating
+* John Resig - http://ejohn.org/ - MIT Licensed
+*
+* PARAMETERS:
+*
+*	@urlTemplate  	- template URL;
+* 	@urlData		- data URL 
+* 	@function		- callback function (optional)
+*
 * USAGE:
 *
-*	$('#foo').vkTemplate('myTemplate.tmpl','myData.php', function({...}));
-*   $('#foo').vkTemplate('myTemplate.tmpl','myData.php', myCallback);
+*	$('#container').vkTemplate('myTemplate.tmpl','myData.php', function({...}));
+*   $('#container').vkTemplate('myTemplate.tmpl','myData.php', myCallback);
 *		
-**/
+*/
+
 (function(jQuery) {
 
 	var templates = {};
@@ -59,16 +68,19 @@
 	};
 	
 //-------------------------------
-// Simple JavaScript Templating
-// John Resig - http://ejohn.org/ - MIT Licensed
+/*
+ * Simple JavaScript Templating. 
+ * Written by John Resig - http://ejohn.org/ - MIT Licensed.
+ *
+ * Modified by Vadim Kiryukhin:
+ * a) removed unnessesary (inside this plugin) conditions.
+ * b) to resolves a "single quote" issue, str.replace() is modified according Neil's comment at 
+ * http://www.west-wind.com/weblog/posts/2008/Oct/13/Client-Templating-with-jQuery
+ */
 
-	var cache = {};
-  
+	
 	function tmpl(str, data){
-		var fn = !/\W/.test(str) ?
-		cache[str] = cache[str] ||
-        tmpl(document.getElementById(str).innerHTML) :
-		new Function("obj",
+		var fn = new Function("obj",
         "var p=[],print=function(){p.push.apply(p,arguments);};" +
         "with(obj){p.push('" +
         str.replace(/[\r\t\n]/g, " ")
@@ -79,8 +91,9 @@
 			.split("<%").join("');")
 			.split("%>").join("p.push('")
 			+ "');}return p.join('');");
-		return data ? fn( data ) : fn;
+		return fn( data );
 	};
+	
  //-----------------------------	
 	
 })(jQuery);
