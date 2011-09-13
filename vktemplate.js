6f7b1f356d320a8a9ee8a4d224876eb7a1b33b5d
unknown <vkiryukhin@.corp.valueclick.com>
2011-09-12 18:44:17 -0700

    simplified tmpl() function by removing unnessesary conditions.Rename the function and cache var. moved the function up.

--- vktemplate.js
+++ vktemplate.js
@@ -10,9 +10,11 @@
 *   http://www.opensource.org/licenses/mit-license.php
 *   http://www.gnu.org/licenses/gpl.html
 *
-* Includes Simple JavaScript Templating
-* John Resig - http://ejohn.org/ - MIT Licensed
-*
+* _tmpl function is "Micro-Templating" engine, 
+* originally written by John Resig ( http://ejohn.org/ - MIT Licensed )
+* and modified by Vadim Kiryukhin based on Neil's comment at 
+* http://www.west-wind.com/weblog/posts/2008/Oct/13/Client-Templating-with-jQuery
+* 
 * PARAMETERS:
 *
 *	@urlTemplate  	- template URL;
@@ -28,18 +30,33 @@
 
 (function(jQuery) {
 
-	var templates = {};
+	var vkTemplatesCache = {};
 	
 	jQuery.fn.vkTemplate = function (urlTmpl, urlData, callback ) {
 	
-		function _getData(urlData, elm, callback) {
+		function _tmpl(str, data){ //modified Micro-Templating engine
+			var fn = new Function("obj",
+			"var p=[],print=function(){p.push.apply(p,arguments);};" +
+			"with(obj){p.push('" +
+			str.replace(/[\r\t\n]/g, " ")
+				.replace(/'(?=[^%]*%>)/g,"\t")
+				.split("'").join("\\'")
+				.split("\t").join("'")
+				.replace(/<%=(.+?)%>/g, "',$1,'")
+				.split("<%").join("');")
+				.split("%>").join("p.push('")
+				+ "');}return p.join('');");
+			return fn( data );
+		};
+	
+		function _getData(urlData, elm, callback) { // get data with ajax
 		
 			$.ajax( {
 				url		: urlData,
 				dataType: "text",
 				cache	: false, 
 				success	: function(data) {
-					$(elm).empty().append(tmpl(templates[urlTmpl],jQuery.parseJSON(data)));	
+					$(elm).empty().append(_tmpl(vkTemplatesCache[urlTmpl],jQuery.parseJSON(data)));	
 					if(typeof(callback) === 'function') {
 						callback(elm);
 					}
@@ -50,7 +67,7 @@
 		return this.each(function () {
 			var elm = this;
 
-			if(templates[urlTmpl]) { //template has been cashed;
+			if(vkTemplatesCache[urlTmpl]) { //template has been cashed;
 				_getData(urlData, elm, callback);
 				
 			} else { //get template with ajax
@@ -59,41 +76,12 @@
 					url: urlTmpl,
 					dataType: "text",
 					success: function(data) { 
-						templates[urlTmpl] = data; // save template in cache
+						vkTemplatesCache[urlTmpl] = data; // save template in cache
 						_getData(urlData, elm, callback);
 					}
 				});
 			}
 		});
 	};
-	
-//-------------------------------
-/*
- * Simple JavaScript Templating. 
- * Written by John Resig - http://ejohn.org/ - MIT Licensed.
- *
- * Modified by Vadim Kiryukhin:
- * a) removed unnessesary (inside this plugin) conditions.
- * b) to resolves a "single quote" issue, str.replace() is modified according Neil's comment at 
- * http://www.west-wind.com/weblog/posts/2008/Oct/13/Client-Templating-with-jQuery
- */
 
-	
-	function tmpl(str, data){
-		var fn = new Function("obj",
-        "var p=[],print=function(){p.push.apply(p,arguments);};" +
-        "with(obj){p.push('" +
-        str.replace(/[\r\t\n]/g, " ")
-		    .replace(/'(?=[^%]*%>)/g,"\t")
-			.split("'").join("\\'")
-			.split("\t").join("'")
-			.replace(/<%=(.+?)%>/g, "',$1,'")
-			.split("<%").join("');")
-			.split("%>").join("p.push('")
-			+ "');}return p.join('');");
-		return fn( data );
-	};
-	
- //-----------------------------	
-	
 })(jQuery);
