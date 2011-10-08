c7fe0f95f855130ca81aad0282f7f6df31a39f91
Vadim Kiryukhin <vkiryukhin@gmail.com>
2011-10-08 14:14:56 -0700

    et version to 0.91.01.beta

--- vktemplate.js
+++ vktemplate.js
@@ -1,7 +1,7 @@
 /**
 * vkTemplate - jQuery Plugin
 *  
-* Version - 0.91.00.beta ( ECMAScript 5 strict mode compatible, no with() statement.)
+* Version - 0.91.01.beta ( ECMAScript 5 strict mode compatible, no with() statement.)
 * Copyright (c) 2010 - 2011 Vadim Kiryukhin
 * vkiryukhin @ gmail.com
 * http://www.eslinstructor.net/vktemplate/
