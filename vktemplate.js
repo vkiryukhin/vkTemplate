fdf90b642d9956190ba8fdb1fe73ed872a19b9e8
Vadim Kiryukhin <vkiryukhin@gmail.com>
2011-10-09 02:19:17 -0700

    precompile template and save function in cache

--- vktemplate.js
+++ vktemplate.js
@@ -1,7 +1,7 @@
 /**
 * vkTemplate - jQuery Plugin
 *  
-* Version - 0.91.01.beta ( ECMAScript 5 strict mode compatible, no with() statement.)
+* Version - 0.92.00.beta ( ECMAScript 5 strict mode compatible, no with() statement.)
 * Copyright (c) 2010 - 2011 Vadim Kiryukhin
 * vkiryukhin @ gmail.com
 * http://www.eslinstructor.net/vktemplate/
@@ -47,25 +47,25 @@
 	
 	jQuery.fn.vkTemplate = function (urlTmpl, jsonData, params, callback ) {
 
-		function _tmpl(str, data){ 
-			var fn = new Function("o",
-				"var p=[],print=function(){p.push.apply(p,arguments);};" +
-				"	p.push('" +
+		function _tmpl(str){ 
+			var fn = "var p=[]; p.push('" +
 				str.replace(/[\r\t\n]/g, " ")
-				.replace(/'(?=[^%]*%>)/g,"\t")
-				.split("'").join("\\'")
-				.split("\t").join("'")
-				.replace(/<%=(.+?)%>/g, "',$1,'")
-				.split("<%").join("');")
-				.split("%>").join("p.push('")
-				+ "');    return p.join('');");
-			return fn( data );
+					.replace(/'(?=[^%]*%>)/g,"\t")
+					.split("'").join("\\'")
+					.split("\t").join("'")
+					.replace(/<%=(.+?)%>/g, "',$1,'")
+					.split("<%").join("');")
+					.split("%>").join("p.push('")
+				 + "');return p.join('');";
+			return new Function("o", fn);
 		};
 	
 		function _getData(jsonData, elm, params, callback) { 
 		
 			// both "params" and "callback" arguments are optional, so let's check 
 			//if the 3rd argument exists and either it is an object or a function.
+			if(!jsonData) return;
+			
 			if ( params ) {
 				if ( jQuery.isFunction( params ) ) {// We assume that it's the callback function
 					callback = params;
@@ -77,7 +77,7 @@
 	
 			// jsonData can be: object | string | URL
 			if( typeof(jsonData) === 'object') {//json object
-				$(elm).empty().append(_tmpl(vkTemplatesCache[urlTmpl],jsonData));	
+				$(elm).empty().append(vkTemplatesCache[urlTmpl](jsonData));					
 				if(callback) {
 					callback(elm, jsonData);
 				}
@@ -85,7 +85,7 @@
 			
 			if($.trim(jsonData).charAt(0) == '{') { //JSON-string
 				var jsonObj = jQuery.parseJSON(jsonData);
-				$(elm).empty().append(_tmpl(vkTemplatesCache[urlTmpl],jsonObj));	
+				$(elm).empty().append(vkTemplatesCache[urlTmpl](jsonObj));					
 				if(callback) {
 					callback(elm, jsonObj);
 				}
@@ -100,7 +100,7 @@
 					context : elm,
 					success	: function(data) {
 						var jsonObj = jQuery.parseJSON(data);
-						$(elm).empty().append(_tmpl(vkTemplatesCache[urlTmpl],jsonObj));	
+						$(elm).empty().append(_tmpl(vkTemplatesCache[urlTmpl](jsonObj)));	
 						if(callback) {
 							callback(elm, jsonObj);
 						}
@@ -121,7 +121,7 @@
 					url: urlTmpl,
 					dataType: "text",
 					success: function(data) { 
-						vkTemplatesCache[urlTmpl] = data; // save template in cache
+						vkTemplatesCache[urlTmpl] = _tmpl(data); // compile and save function in cache
 						_getData(jsonData, elm, params, callback);
 					}
 				});
